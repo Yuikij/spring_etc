b8f207520e117b81740203812c5c4436c75b66ea
zxj <775718540@qq.com>
2022-06-16 19:41:32 +0800

    ioc

--- spring_ioc/src/main/java/com/spring/test/TestApplication.java
+++ spring_ioc/src/main/java/com/spring/test/TestApplication.java
@@ -1,17 +1,37 @@
 package com.spring.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class TestApplication {
 
+//
+//    @Autowired
+//    private sLock sLock;
+
+    @Autowired
+    private Lock lock;
+
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
-//
-//    @Bean(initMethod = "initMethod",destroyMethod = "destroyMethod")
-//    public BeanTest beanTest(){
-//        return new BeanTest();
-//    }
+
+
+
+
 }
+
+@Configuration
+class Config{
+    @Bean
+    public Lock Lock(){
+        return new sLock();
+    }
+}
+interface Lock{}
+
+class sLock implements Lock{}
