2128ca0fe9391712751542b92e9136d6c671b171
zxj <775718540@qq.com>
2022-10-24 15:00:11 +0800

    lc

--- java_test/src/main/java/aki/guava/BloomFilterT.java
+++ java_test/src/main/java/aki/guava/BloomFilterT.java
@@ -4,5 +4,6 @@ import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnels;
 
 public class BloomFilterT {
-    BloomFilter<Integer> friends = BloomFilter.create(Funnels.integerFunnel(), 500, 0.01);
+    BloomFilter<Integer> friends =
+            BloomFilter.create(Funnels.integerFunnel(), 500, 0.01);
 }
--- java_test/src/main/java/aki/guava/OptionalT.java
+++ java_test/src/main/java/aki/guava/OptionalT.java
@@ -3,14 +3,17 @@ package aki.guava;
 
 import com.google.common.base.Strings;
 
+import java.util.Collections;
+import java.util.Optional;
+
 public class OptionalT {
     public static void main(String[] args) {
         Integer a = 1;
-        com.google.common.base.Optional<Integer> possible = com.google.common.base.Optional.fromNullable(a);
-        System.out.println(possible.or(2));
-        System.out.println(possible.orNull());
+        Optional<Integer> possible = Optional.ofNullable(a);
+        System.out.println(possible.orElse(2));
+        System.out.println(possible.orElse(null));
         System.out.println(com.google.common.base.Optional.absent());
-        System.out.println(possible.asSet());
+        System.out.println(possible.map(Collections::singleton).orElse(Collections.emptySet()));
         System.out.println(Strings.nullToEmpty(null));
         System.out.println(Strings.emptyToNull(""));
     }
