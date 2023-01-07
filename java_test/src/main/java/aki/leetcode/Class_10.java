9922f43a6656533a149f650edb9e1f6197b06113
zxj <775718540@qq.com>
2023-01-08 00:10:29 +0800

    10

---
+++ java_test/src/main/java/aki/leetcode/Class_10.java
@@ -0,0 +1,17 @@
+package aki.leetcode;
+//给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+//
+//        '.' 匹配任意单个字符
+//        '*' 匹配零个或多个前面的那一个元素
+//        所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+
+public class Class_10 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isMatch(String s, String p) {
+        return false;
+    }
+}
