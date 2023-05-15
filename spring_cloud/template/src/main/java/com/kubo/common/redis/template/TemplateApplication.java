7e3fdd1621480b9452c4b7381d2d56b7f654c5b8
zxj <775718540@qq.com>
2023-05-15 20:20:20 +0800

    redis

--- spring_cloud/template/src/main/java/com/kubo/common/redis/template/TemplateApplication.java
+++ spring_cloud/template/src/main/java/com/kubo/common/redis/template/TemplateApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 模板模块，用于一些测试
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.kubo"})
 public class TemplateApplication {
--- spring_cloud/template/src/test/java/com/kubo/common/redis/template/TemplateApplicationTests.java
+++ spring_cloud/template/src/test/java/com/kubo/common/redis/template/TemplateApplicationTests.java
@@ -1,11 +1,14 @@
 package com.kubo.common.redis.template;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.kubo.common.redis.service.RedisService;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 @SpringBootTest
@@ -15,10 +18,31 @@ class TemplateApplicationTests {
     @Autowired
     private RedisService redisService;
 
+    @Resource
+    private RedissonClient redissonClient;
+
+
     @Test
-    void contextLoads() {
+    void baseRedisTest() {
         redisService.setCacheObject("123", "kubo");
         System.out.println(redisService.getCacheObject("123").toString());
+    }
+
+    /**
+     * 分布式锁测试
+     */
+    @Test
+    void distributedLock() throws InterruptedException {
+        RLock lock = redissonClient.getLock("client_test");
+        boolean isLocked  = lock.tryLock();
+        if (isLocked ){
+            System.out.println("isLocked ");
+            Thread.sleep(1000000);
+        }else{
+            Thread.sleep(1000);
+            distributedLock();
+        }
+
     }
 
 }
