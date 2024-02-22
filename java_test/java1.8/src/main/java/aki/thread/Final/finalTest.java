d7847b7afe379120534af66408b0940d864d7b65
zxj <775718540@qq.com>
2024-02-22 17:01:33 +0800

    final

---
+++ java_test/java1.8/src/main/java/aki/thread/Final/finalTest.java
@@ -0,0 +1,24 @@
+package aki.thread.Final;
+
+public class finalTest {
+    class Foo {
+        final int a;
+        int b;
+
+        Foo() {
+            this.a = 1;
+            b = 2;
+        }
+    }
+
+    public static void write(){
+
+    }
+
+    public static void read(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
