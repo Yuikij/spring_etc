3e40fc8c89cb368425a7bb2104c6f8f8d5893a5e
zxj <775718540@qq.com>
2024-03-07 17:44:18 +0800

    线程池

--- java_test/java1.8/src/main/java/aki/并发/threadPool/ScheduledThreadPoolTest.java
+++ java_test/java1.8/src/main/java/aki/并发/threadPool/ScheduledThreadPoolTest.java
@@ -6,6 +6,7 @@ import com.google.common.base.Stopwatch;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class ScheduledThreadPoolTest {
@@ -18,22 +19,22 @@ public class ScheduledThreadPoolTest {
         stopwatch.start();
         stopWatch.start("t1");
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
-        ScheduledExecutorService newSingleThreadScheduledExecutor = Executors.newSingleThreadScheduledExecutor();
-
-//        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor();
+//        ScheduledExecutorService newSingleThreadScheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+//
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(2);
 //        scheduledExecutorService.schedule(() -> {
-//            Utils.print("start 1 : " + stopwatch.elapsed().toMillis());
-//            Utils.sleep(1000);
-//            Utils.print("end : " + stopwatch.elapsed().toMillis());
+//            ConcurrentUtils.print("start 1 : " + stopwatch.elapsed().toMillis());
+//            ConcurrentUtils.sleep(1000);
+//            ConcurrentUtils.print("end : " + stopwatch.elapsed().toMillis());
 //        }, 1, TimeUnit.SECONDS);
 
 
-//周期性执行任务，间隔时间考虑任务时间
-        scheduledExecutorService.scheduleAtFixedRate(() -> {
-            ConcurrentUtils.print("start 2 : " + stopwatch.elapsed().toMillis());
-            ConcurrentUtils.sleep(2000);
-            ConcurrentUtils.print("end 2 : " + stopwatch.elapsed().toMillis());
-        },1,1L,TimeUnit.SECONDS);
+////周期性执行任务，间隔时间考虑任务时间
+//        scheduledExecutorService.scheduleAtFixedRate(() -> {
+//            ConcurrentUtils.print("start 2 : " + stopwatch.elapsed().toMillis());
+//            ConcurrentUtils.sleep(2000);
+//            ConcurrentUtils.print("end 2 : " + stopwatch.elapsed().toMillis());
+//        },1,1L,TimeUnit.SECONDS);
 
 
 //周期性执行任务，间隔时间是两个任务的间隔
